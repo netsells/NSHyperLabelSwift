d8872e002117e0568d32b1f317112d4f9a065725
Jack Colley <jack.colley@netsells.co.uk>
2016-07-20 11:53:35 +0100

    Adding base UILabel class

---
+++ NSHyperLabelSwift/Classes/NSHyperLabel.swift
@@ -0,0 +1,21 @@
+//
+//  NSHyperLabel.swift
+//  Pods
+//
+//  Created by Jack Colley on 20/07/2016.
+//
+//
+
+import UIKit
+
+class NSHyperLabel: UILabel {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
